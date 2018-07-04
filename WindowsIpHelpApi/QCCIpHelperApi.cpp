74a2da88f733e856a75a9092b4777bf15e15cb38
Chuncheng666 <1015830270@qq.com>
2018-07-04 23:13:17 +0800

    获取本地计算机IP地址表

--- WindowsIpHelpApi/QCCIpHelperApi.cpp
+++ WindowsIpHelpApi/QCCIpHelperApi.cpp
@@ -9,6 +9,7 @@
 #include <IPHlpApi.h>
 
 #pragma comment(lib, "IPHLPAPI.lib")
+#pragma comment(lib, "ws2_32.lib")
 
 #define HPMALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
 #define HPFREE(x) HeapFree(GetProcessHeap(), 0, (x))
@@ -180,12 +181,92 @@ void QCCShowInterfaceDetailInfo()
 	HPFREE(pInterFaceInfo);
 }
 
+void QCCShowLocalIpAddressList()
+{
+	PMIB_IPADDRTABLE pIpAddrTableInfo;
+	ULONG ipTableSize;
+	DWORD errCode;
+	IN_ADDR ipAddr;
+	LPVOID lpErrorMsg;
+
+	pIpAddrTableInfo = (MIB_IPADDRTABLE*)HPMALLOC(sizeof(MIB_IPADDRTABLE));
+	if (pIpAddrTableInfo)
+	{
+		if (errCode = GetIpAddrTable(pIpAddrTableInfo, &ipTableSize, 0) == ERROR_INSUFFICIENT_BUFFER)
+		{
+			HPFREE(pIpAddrTableInfo);
+			pIpAddrTableInfo = (MIB_IPADDRTABLE*)HPMALLOC(ipTableSize);
+		}
+
+		if (pIpAddrTableInfo == NULL)
+		{
+			cout << "无法分配内存空间给MIB_IPADDRTABLE ! " << endl;
+			return;
+		}
+	}
+
+	if (errCode = GetIpAddrTable(pIpAddrTableInfo, &ipTableSize, 0) != NO_ERROR)
+	{
+		cout << "本地计算机Ip地址表获取失败 ! " << endl;
+		if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, errCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpErrorMsg, 0, NULL))
+		{
+			cout << " 错误信息 : " << lpErrorMsg << endl;
+			LocalFree(lpErrorMsg);
+			HPFREE(pIpAddrTableInfo);
+			pIpAddrTableInfo = NULL;
+			return;
+		}
+	}
+	else
+	{
+		cout << "所有查询记录数量: " << pIpAddrTableInfo->dwNumEntries << endl;
+		for (DWORD i = 0; i < pIpAddrTableInfo->dwNumEntries; i++)
+		{
+			cout << "接口序号[" << i << "] : " << pIpAddrTableInfo->table[i].dwIndex << endl;
+
+			ipAddr.S_un.S_addr = (ULONG)pIpAddrTableInfo->table[i].dwAddr;
+			cout << "IP地址[" << i << "] : " << inet_ntoa(ipAddr) << endl;
+			ipAddr.S_un.S_addr = (ULONG)pIpAddrTableInfo->table[i].dwMask;
+			cout << "子网掩码[" << i << "] : " << inet_ntoa(ipAddr) << endl;
+			ipAddr.S_un.S_addr = (ULONG)pIpAddrTableInfo->table[i].dwBCastAddr;
+			cout << "广播地址[" << i << "] : " << inet_ntoa(ipAddr) << endl;
+
+			cout << "重组报文最大数量[" << i << "] : " << pIpAddrTableInfo->table[i].dwReasmSize << endl;
+			cout << "类型和状态[" << i << "] : ";
+
+			if (pIpAddrTableInfo->table[i].wType & MIB_IPADDR_PRIMARY)
+				cout << "主IP地址! ";
+
+			if (pIpAddrTableInfo->table[i].wType & MIB_IPADDR_DYNAMIC)
+				cout << "动态IP地址! ";
+
+			if (pIpAddrTableInfo->table[i].wType & MIB_IPADDR_DISCONNECTED)
+				cout << "断开连接的接口对应的IP地址! ";
+
+			if (pIpAddrTableInfo->table[i].wType & MIB_IPADDR_DELETED)
+				cout << "删除的IP地址! ";
+
+			if (pIpAddrTableInfo->table[i].wType & MIB_IPADDR_TRANSIENT)
+				cout << "临时地址！ ";
+
+			cout << endl << endl;
+		}
+	}
+
+	if (pIpAddrTableInfo)
+	{
+		HPFREE(pIpAddrTableInfo);
+		pIpAddrTableInfo = NULL;
+	}
+}
+
 int main()
 {
 	QCCShowAllAdaptersInfo();
 	QCCShowNetworkParams();
 	QCCShowInterfaceNumInfo();
 	QCCShowInterfaceDetailInfo();
+	QCCShowLocalIpAddressList();
 	getchar();
     return 0;
 }
