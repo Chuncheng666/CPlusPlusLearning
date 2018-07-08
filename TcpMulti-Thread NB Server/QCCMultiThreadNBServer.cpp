18f044ff3c327705924bc0afe3a03bc05c8b0dfa
Chuncheng666 <1015830270@qq.com>
2018-07-08 22:57:20 +0800

    多线程服务端，允许多客户端连接

--- TcpMulti-Thread NB Server/QCCMultiThreadNBServer.cpp
+++ TcpMulti-Thread NB Server/QCCMultiThreadNBServer.cpp
@@ -1,4 +1,4 @@
-// NonBlockingTCPServer.cpp : Defines the entry point for the console application.
+// QCCMultiThreadNBServer.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -29,6 +29,74 @@ string QccGetCurrentTime()
 	char sTime[30];
 	sprintf(sTime, "%4d-%02d-%02d %02d:%02d:%02d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
 	return sTime;
+}
+
+DWORD WINAPI QCCCommunicateToClientThread(LPVOID lparam)
+{
+	char buffer[BUF_SIZE];
+	int errCode;
+
+	SOCKET qCliSocket = (SOCKET)(LPVOID)lparam;
+	while (true)
+	{
+		ZeroMemory(buffer, BUF_SIZE);
+		errCode = recv(qCliSocket, buffer, BUF_SIZE, 0);
+		if (errCode == SOCKET_ERROR)
+		{
+			int er = WSAGetLastError();
+			if (er == WSAEWOULDBLOCK)
+			{
+				cout << "No data from buffer on non blocking mode begining!" << endl;
+				Sleep(1000);
+				continue;
+			}
+			else
+			{
+				cout << "Recv failed ! Error code is : " << WSAGetLastError() << endl;
+				closesocket(qCliSocket);
+				WSACleanup();
+				return -1;
+			}
+		}
+
+		string curTime = QccGetCurrentTime();
+		cout << curTime << ", Recv from client: " << buffer << endl;
+
+		if (strcmp(buffer, "quit") == 0)
+		{
+			errCode = send(qCliSocket, "quit", strlen("quit"), 0);
+			break;
+		}
+		else
+		{
+			char srvMessage[BUF_SIZE];
+			sprintf(srvMessage, "Message recv -- %s", buffer);
+			while (true)
+			{
+				errCode = send(qCliSocket, srvMessage, sizeof(srvMessage), 0);
+				if (errCode == SOCKET_ERROR)
+				{
+					int errr = WSAGetLastError();
+					if (errr == WSAEWOULDBLOCK)
+					{
+						cout << "can not finish non blocking mode opreation !" << endl;
+						Sleep(1000);
+						continue;
+					}
+					else
+					{
+						cout << "Send failed ! Error code is : " << WSAGetLastError() << endl;
+						closesocket(qCliSocket);
+						WSACleanup();
+						return -1;
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	closesocket(qCliSocket);
 }
 
 int main()
@@ -124,92 +192,15 @@ int main()
 				Sleep(1000);
 				continue;
 			}
-			else
-			{
-				cout << "Socket accept error ! Error code is : " << WSAGetLastError() << endl;
-				closesocket(qSrvSocket);
-				WSACleanup();
-				getchar();
-				return -1;
-			}
-			break;
 		}
 		else
 		{
 			cout << "Socket connect address info is : " << inet_ntoa(qCliAddr.sin_addr) << endl;
-			getchar();
+			DWORD qTheadId;
+			CreateThread(NULL, NULL, QCCCommunicateToClientThread, (LPVOID)qCliSocket, 0, &qTheadId);
 		}
 	}
-
-	//whileÑ­»·½ÓÊÕ
-	while (true)
-	{
-		ZeroMemory(buffer, BUF_SIZE);
-		errCode = recv(qCliSocket, buffer, BUF_SIZE, 0);
-		if (errCode == SOCKET_ERROR)
-		{
-			int er = WSAGetLastError();
-			if (er == WSAEWOULDBLOCK)
-			{
-				cout << "No data from buffer on non blocking mode begining!" << endl;
-				Sleep(1000);
-				continue;
-			}
-			else
-			{
-				cout << "Recv failed ! Error code is : " << WSAGetLastError() << endl;
-				closesocket(qSrvSocket);
-				closesocket(qCliSocket);
-				WSACleanup();
-				return -1;
-			}
-		}
-
-		string curTime = QccGetCurrentTime();
-		cout << curTime << ", Recv from client[ " << inet_ntoa(qCliAddr.sin_addr) << ":" << qCliAddr.sin_port << "] : " << buffer << endl;
-
-		if (strcmp(buffer, "quit") == 0)
-		{
-			errCode = send(qCliSocket, "quit", strlen("quit"), 0);
-			break;
-		}
-		else
-		{
-			char srvMessage[BUF_SIZE];
-			sprintf(srvMessage, "Message recv -- %s", buffer);
-			while (true)
-			{
-				errCode = send(qCliSocket, srvMessage, sizeof(srvMessage), 0);
-				if (errCode == SOCKET_ERROR)
-				{
-					int errr = WSAGetLastError();
-					if (errr == WSAEWOULDBLOCK)
-					{
-						cout << "can not finish non blocking mode opreation !" << endl;
-						Sleep(1000);
-						continue;
-					}
-					else
-					{
-						cout << "Send failed ! Error code is : " << WSAGetLastError() << endl;
-						closesocket(qSrvSocket);
-						closesocket(qCliSocket);
-						WSACleanup();
-						return -1;
-					}
-				}
-				break;
-			}
-		}
-	}
-
-	closesocket(qSrvSocket);
-	closesocket(qCliSocket);
-	WSACleanup();
-
-	getchar();
 
 	return 0;
 }
-
 
